913e612e9ac7a359f8ff9918dab4d3ffa7782a4a
AnshuGoyal <anshu.goyal9194@gmail.com>
2018-06-05 18:12:35 +0530

    Server Setup

--- app/server/index.js
+++ app/server/index.js
@@ -1,6 +1,14 @@
 import app from './app';
+const http               = require('http');
 
 const port = process.env.PORT || 8000;
 
-app.listen(port);
-console.log(`Listening at http://localhost:${port}`);
+const server = http.createServer(app);
+//
+// app.listen(port);
+// console.log(`Listening at http://localhost:${port}`);
+server.listen(port, (err) => {
+  	if ( ! err) {
+  		console.log(`server is listening on `+port);
+  	}
+  })
