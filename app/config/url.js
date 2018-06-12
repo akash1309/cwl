6348133f98febbf4cfb065be11c3c1596c577395
akash1309 <akash.goyal1309@gmail.com>
2018-06-12 15:04:42 +0530

    all DyCEE function implemented on CEE page

--- app/config/url.js
+++ app/config/url.js
@@ -3,3 +3,4 @@ export const baseUrl = "http://localhost:8000";
 export const validateUrl = "/validate/";
 export const signupUrl = "/signUp/";
 export const loginUrl= "/login/";
+export const allDyCeeUrl="/dycee/all/";
--- app/server/routes.js
+++ app/server/routes.js
@@ -1,5 +1,6 @@
 const routes = [
-    '/'
+    '/',
+    '/cee'
 ]
 
 export default routes;
