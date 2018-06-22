f291f686cdd7c569d7c36da6d8b195760bcf2796
akash1309 <akash.goyal1309@gmail.com>
2018-06-22 12:52:25 +0530

    links added in url.js

--- app/config/url.js
+++ app/config/url.js
@@ -1,24 +1,57 @@
 export const baseUrl = "http://localhost:8000";
 
+////////////////////////////// DYCEE ////////////////////////////
+export const allDyCeeUrl="/dycee/all/";
+export const addDyCEEUrl="/dycee/add/";
+export const oneDyCeeUrl="/dycee/one/";
+export const someDyCeeUrl="/dycee/some/";
+
+///////////////////////////// INSPECTOR ////////////////////////
+export const allInspectorUrl="/inspector/all/";
+export const inspectorUrl="/inspector/add/";
+export const oneInspectorUrl="/inspector/one/";
+export const someInspectorUrl="/inspector/some/";
+
+///////////////////////////// STORE OFFICER ///////////////////
+export const allStoreOfficerUrl="/storeofficer/all/";
+export const storeofficerUrl="/storeofficer/add/";
+export const oneStoreOfficerUrl="/storeofficer/one/";
+export const someStoreOfficerUrl="/storeofficer/some/";
+
+//////////////////////// PURCHASE ORDER //////////////////////
+export const addPurchaseOrderUrl ="/purchaseorder/add/";
+export const allPurchaseOrderUrl = '/purchaseorder/all/';
+export const onePurchaseOrderUrl="/order/get/";
+
+////////////////////////// VENDOR //////////////////////
+export const addVendorUrl ="/vendor/add/";
+export const allVendorUrl="/vendor/all/";
+export const oneVendorUrl="/vendor/one/";
+export const someVendorUrl="/vendor/some/";
+
+////////////////// INSPECTION CERTIFICATE(IC) ////////////////
+export const icGenerateUrl = '/ic/generate/';
+export const allIcUrl = '/showIC/all/';
+export const oneIcUrl ="/showIC/one/";
+
+//////////////////////// CORRIGENDUM //////////////////
+export const allCorrigendumUrl ='/corrigendum/showCorrigendum/all/';
+export const generateCorrigendumUrl = '/corrigendum/generate/';
+export const oneCorrigendumUrl ="corrigendum/showCorrigendum/one";
+
+///////////////////// INSPECTION REPORT //////////////////
+export const allIrUrl = '/showIR/';
+export const addInspectionReportUrl = '/irStatus/set/';
+export const oneInspectionReportUrl = "/irStatus/get/:order_number/:ic_id/";
+
+///////////////////// ITEMS //////////////////////
+export const addItemUrl = '/items/add/';
+export const allItemUrl = '/showItems/all/';
+export const oneItemUrl = '/showItems/one/';
+
+//////////////////// OTHERS /////////////////////
 export const validateUrl = "/validate/";
 export const signupUrl = "/signUp/";
 export const loginUrl= "/login/";
-export const allDyCeeUrl="/dycee/all/";
-export const allInspectorUrl="/inspector/all/";
-export const allStoreOfficerUrl="/storeofficer/all/";
-export const allVendorUrl="/vendor/all/";
-export const addDyCEEUrl="/dycee/add/";
-export const addPurchaseOrderUrl ="/purchaseorder/add/";
-export const addVendorUrl ="/vendor/add/";
-export const inspectorUrl="/inspector/add/";
 export const getInfoUrl = "/user/";
 export const updateInfoUrl = '/updateinfo/';
-export const allPurchaseOrderUrl = '/purchaseorder/all/';
-export const addItemUrl = '/items/add/';
-export const allItemUrl = '/showItems/';
-export const allCorrigendumUrl ='/corrigendum/showCorrigendum/';
-export const generateCorrigendumUrl = '/corrigendum/generate/';
-export const icGenerateUrl = '/ic/generate/';
-export const allIcUrl = '/showIC/';
-export const allIrUrl = '/showIR/';
-export const addInspectionReportUrl = '/irStatus/set/';
