55c2f278bcfdfdfa610c9fd4091289c031df5e84
Shobha Goyal <shobha4goyal@gmail.com>
2018-07-22 16:00:45 +0530

    baseUrl also updated to 8080 in url.js

--- app/config/url.js
+++ app/config/url.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8000";
+export const baseUrl = "http://localhost:8080";
 
 ////////////////////////////// DYCEE ////////////////////////////
 export const allDyCeeUrl="/dycee/all/";
